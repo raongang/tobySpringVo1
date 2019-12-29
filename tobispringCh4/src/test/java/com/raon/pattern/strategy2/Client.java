2842e8f3c99d0569ae26481c1577f4c6ada996de
raongang <sayllclubs@naver.com>
2019-12-30 08:44:19 +0900

    2019-12-30

--- tobispringCh4/src/test/java/com/raon/pattern/strategy2/Client.java
+++ tobispringCh4/src/test/java/com/raon/pattern/strategy2/Client.java
@@ -4,10 +4,12 @@ package com.raon.pattern.strategy2;
 /***
  * 
  * @author admin
- * 
- *
+ *   Strategy Pattern
+ *       - 전략객체 ( interface )
+ *       - 전략객체를 이용하는 컨텍스트
+ *       - 전략객체를 생성 컨텍스트에 주입하는 클라이언트
+ *     
  */
-
 
 //전략생성 및 context에게 주입
 public class Client {
