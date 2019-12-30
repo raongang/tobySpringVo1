37db7f5128435c1170ebc9dbd7d10a9ca8afd601
raongang <sayllclubs@naver.com>
2019-12-30 17:28:59 +0900

    2019-12-30 client update

--- tobispringCh4/src/test/java/com/raon/pattern/strategy2/Client.java
+++ tobispringCh4/src/test/java/com/raon/pattern/strategy2/Client.java
@@ -5,10 +5,10 @@ package com.raon.pattern.strategy2;
  * 
  * @author admin
  *   Strategy Pattern
- *       - 전략객체 ( interface )
+ *       - 전략객체 (interface)
  *       - 전략객체를 이용하는 컨텍스트
  *       - 전략객체를 생성 컨텍스트에 주입하는 클라이언트
- *     
+ *       - 
  */
 
 //전략생성 및 context에게 주입
@@ -19,7 +19,7 @@ public class Client {
 		Solider rambo = new Solider();
 		strategy = new StrategtGun();
 		
-		rambo.runContext( strategy);
+		rambo.runContext(strategy);
 		System.out.println("\n");
 		
 		strategy = new StrategyGrenade();
