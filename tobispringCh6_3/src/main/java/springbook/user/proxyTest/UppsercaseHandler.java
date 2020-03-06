423e133822c6f7dbf290766674c8d39589647abf
kyeonghunYoon <erjk586@gmail.com>
2020-03-06 15:56:22 +0900

    2020-03-06 raongang @Transactional add

---
+++ tobispringCh6_3/src/main/java/springbook/user/proxyTest/UppsercaseHandler.java
@@ -0,0 +1,28 @@
+package springbook.user.proxyTest;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/** dynamic handler */
+public class UppsercaseHandler implements InvocationHandler{
+	Object target;
+	
+	public UppsercaseHandler(Object target) {
+		super();
+		this.target = target;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		Object ret = method.invoke(target, args);
+		
+		//메소드를 선별해서 부가기능 실시.
+		if(ret instanceof String && method.getName().startsWith("say")) {
+			return ((String)ret).toUpperCase();
+		}
+		else {
+			return ret;
+		}
+	}
+}
